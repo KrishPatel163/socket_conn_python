59fec467b01273ebcb1810a4be18cb80ffb2d36a
KrishPatel163 <krishhpatel7587@gmail.com>
2024-11-14 14:41:03 +0530

    final commits

--- client/client.py
+++ client/client.py
@@ -17,49 +17,67 @@ def send_requests():
         if command.startswith("REQUEST_FILE"):
             target_client = input("Enter target client address in format (host, port): ")
             file_name = input("Enter filename to request: ")
+            print(f"Requesting File {file_name}, From {target_client}")
             client_socket.send(f"REQUEST_FILE {target_client} {file_name}".encode())
         
         elif command.upper() == "EXIT":
             client_socket.send("DISCONNECT".encode())
             break
+        elif command.lower().startswith("me"):
+                print(client_socket.getsockname())
 
 def receive_responses():
     while True:
         try:
             response = client_socket.recv(1024).decode()
-            
+            print(response)
             # Conditional Block for the requested file
             if response.startswith("FILE_REQUEST"):
-                _, file_name, requestor_addr = response.split()
-                requestor_addr = ast.literal_eval(requestor_addr)
+                print(response, response.split())
+                _, file_name,requestor_addr = response.split(maxsplit=2)
+                requestor_addr = ast.literal_eval((requestor_addr))
 
                 print(f"Received a file request for {file_name} from {requestor_addr}")
-                if os.path.exists(file_name):
+                file_path = os.path.join("../data/", file_name)
+                if os.path.exists(file_path):
                     client_socket.sendall(f"SENDING_FILE {file_name} {requestor_addr}".encode())
+                    chunk_length = 0
                     with open(f"../data/{file_name}", "rb") as file:
                         chunk = file.read(1024)
+                        print("Sending the data of file of type: ", type(chunk))
                         while chunk:
+                            chunk_length = chunk_length + len(chunk)
+                            if not chunk:
+                                print("Sender recived EOF")
+                                break
                             client_socket.sendall(chunk)
                             chunk = file.read(1024)
-                    client_socket.sendall(b"DONE")
+
+                    client_socket.sendall(b"F-201")
+                    print("Sent chunk of total lenght of: ", chunk_length)
                 else:
                     client_socket.sendall("File not found.".encode())
             
             # Conditional Block for Reciving the incoming
             elif response.startswith("SENDING_FILE"):
                 _, file_name = response.split()
-                print(f"Receiving file '{file_name}'...")
-                
+                print(f"Receiving file '{file_name}'...")                
                 # Create or open the file to write the incoming data
-                timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-                new_file_name = f"{file_name}-{timestamp}"
-                with open(f"../data/received_{new_file_name}", "wb") as file:
-                    while True:
-                        data = client_socket.recv(1024)
-                        if data == b"DONE":  # End of file transmission
-                            print(f"File '{file_name}' received successfully.")
-                            break
-                        file.write(data)
+                recv_chunk_length = 0
+                with open(f"../data/received_{file_name}", "wb") as file:
+                    try:
+                        while True:
+                                data = client_socket.recv(1024)
+                                recv_chunk_length = recv_chunk_length + len(data)
+                                print("Reciving data of file of type: ", type(data))
+                                if data == b"F-201":  # End of file transmission
+                                    print(f"*********File '{file_name}' received successfully.***********")
+                                    break
+                                file.write(data)
+                                print("Requestor recived chunk size of: ",recv_chunk_length)
+                    except Exception as e:
+                        print(f"Error while trying to write: {e}")
+                        break
             else:
                 print("Server:", response)
 
--- server/server.py
+++ server/server.py
@@ -27,12 +27,12 @@ def handle_client(client_socket, client_addr):
     try:
         while True:
             data = client_socket.recv(1024).decode()
+            print(data)
             if not data:
                 break
             if data.startswith("REQUEST_FILE"):
                 _, target_client, file_name = data.split()
-                print(data.split())
-                
+                print(target_client)
                 if target_client.startswith("(") and target_client.endswith(")"):
                     target_client = f"('{target_client[1:].split(',')[0]}',{target_client.split(',')[1]}"
                 
@@ -40,9 +40,33 @@ def handle_client(client_socket, client_addr):
                 
                 if target_client in client_ids:
                     target_client_socket = client_ids[target_client]
-                    target_client_socket.send(f"FILE_REQUEST of {file_name} from {client_addr}".encode())
+                    target_client_socket.send(f"FILE_REQUEST {file_name} {client_addr}".encode())
                 else:
                     client_socket.send("Target client not found.".encode())
+                    
+            elif data.startswith("SENDING_FILE"):
+                _, file_name, requestor_addr = data.split(maxsplit=2)
+                requestor_addr = ast.literal_eval(requestor_addr)
+
+                if requestor_addr in client_ids:
+                    requestor_socket = client_ids[requestor_addr]
+                    # Notify the requester that file transfer is starting
+                    requestor_socket.send(f"SENDING_FILE {file_name}".encode())
+                    
+                    # Relay file data from sender to requester
+                    chunk_size = 0
+                    while True:
+                        chunk = client_socket.recv(1024)
+                        print(chunk)
+                        chunk_size = chunk_size + len(chunk)
+                        if not chunk:  # End of file transmission
+                            print("Server sensed end of file.")
+                            requestor_socket.send(b"F-201")
+                            break
+                        requestor_socket.send(chunk)
+                        print("Server Recived the chunk size of: ",chunk_size)
+                else:
+                    client_socket.send("Requesting client not connected.".encode())
 
             
     except ConnectionResetError:
@@ -50,6 +74,8 @@ def handle_client(client_socket, client_addr):
     finally:
         print(f"Client {client_addr} left the session. ")
         connected_clients.remove(client_addr)
+        available_sockets.remove(client_socket)
+        del client_ids[client_addr]
         print(f"Remaining clients are: {connected_clients}")
         client_socket.close()
 
